4b86bdaa4d7007e3bce8c1a71e53b8c0f3fec4a9
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2023-03-13 09:39:17 +0530

    Dashboard frontemd

--- client/src/pages/admin/Dashboard.js
+++ client/src/pages/admin/Dashboard.js
@@ -1,7 +1,34 @@
+import { Grid } from "@mui/material";
+import BasicCard from "../../components/BasicCard";
+
+import useStyles from "./styles";
+
 const Dashboard = () => {
-    return ( 
-        <div>Dashboard</div>
-     );
-}
- 
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid container spacing={4} className={classes.gridContainer}>
+        <Grid item xs={4}>
+          <BasicCard text="Total Businesses" count="3" />
+        </Grid>
+        <Grid item xs={4}>
+          <BasicCard text="Total Delivery Agents" count="3" />
+        </Grid>
+        <Grid item xs={4}>
+          <BasicCard text="Total Customers" count="3" />
+        </Grid>
+        <Grid item xs={4}>
+          <BasicCard text="Completed Orders" count="3" />
+        </Grid>
+        <Grid item xs={4}>
+          <BasicCard text="Pending Orders" count="3" />
+        </Grid>
+        <Grid item xs={4}>
+          <BasicCard text="Pending Complaints" count="3" />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
 export default Dashboard;
