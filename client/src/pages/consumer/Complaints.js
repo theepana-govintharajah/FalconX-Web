bc6bb675b263334990c68e9e48e2c24333155934
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2023-03-15 11:31:01 +0530

    small edit

--- client/src/pages/consumer/Complaints.js
+++ client/src/pages/consumer/Complaints.js
@@ -1,7 +1,15 @@
+import Sbutton from "../../components/Sbutton";
+
+import { Link } from "react-router-dom";
+
 const Complaints = () => {
-    return ( 
-        <div>Complaints</div>
-     );
-}
- 
+  return (
+    <div>
+      <Link to="/EmployeeRegistration" className="link">
+        <Sbutton text="Add New" btnWidth="25%" />
+      </Link>
+    </div>
+  );
+};
+
 export default Complaints;
